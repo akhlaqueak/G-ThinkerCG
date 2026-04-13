41cabbb7622f66b44015c322c929c4671f781f1a
akhlaque <akhlaque.ak@gmail.com>
2026-04-12 21:38:02 -0400

    updating

--- app_qc/qc_gpu_context.h
+++ app_qc/qc_gpu_context.h
@@ -271,7 +271,6 @@ public:
             // sum to find tasks count
             atomicAdd(dd.total_tasks, dd.wtasks_count[WARP_IDX]);
             atomicAdd(dd.total_cliques, dd.wcliques_count[WARP_IDX]);
-            dd.wcliques_count[WARP_IDX]=0;
         }
 
         if (IDX == 0)
