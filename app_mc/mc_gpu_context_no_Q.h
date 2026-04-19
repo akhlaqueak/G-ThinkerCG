d557ab3b53cd62178a760d500335807723133284
akhlaque <akhlaque.ak@gmail.com>
2026-04-19 00:01:13 -0400

    updating

--- app_mc/mc_gpu_context_no_Q.h
+++ app_mc/mc_gpu_context_no_Q.h
@@ -440,9 +440,9 @@ public:
         for (ull i = st; i < en; i ++)
         {
             if (Brd.labels[i] == Q)
-                if (isOverflow())
-                    generateSubgraphs(so, Brd.vertices[i], H);
-                else
+                // if (isOverflow())
+                //     generateSubgraphs(so, Brd.vertices[i], H);
+                // else
                     generateSubgraphs(so, Brd.vertices[i], Bwr);
         }
     }
