ccf787c9ee504221ce1f0afecb61e57089135caf
akhlaque <akhlaque.ak@gmail.com>
2026-04-21 23:39:41 -0400

    updating

--- system/master.h
+++ system/master.h
@@ -96,7 +96,7 @@ public:
 
             if (dynamic_cast<GPUWorkerT *>(worker))
             {
-                cout<<"SC size: "<<SC->size()<<endl;
+                cout << "SC size: " << SC->size() << endl;
                 if (not data_array.empty())
                     for (ui i = 0; i < worker->tasks_per_fetch && data_array.size(); i++)
                     {
@@ -122,8 +122,9 @@ public:
                 if (not is_SC_empty())
                 {
                     unique_lock<shared_timed_mutex> lock(SC_mtx);
-
-                    for (ui i = 0; i < worker->tasks_per_fetch && !SC->empty(); i++)
+                    ui chunk = std::min(SC->size() / (worker_list.size() + 1), worker->tasks_per_fetch);
+                    chunk = max(chunk, 1);
+                    for (ui i = 0; i < chunk && !SC->empty(); i++)
                     {
                         TaskT *task = SC->top();
                         worker->Lt.push_back(task);
@@ -143,7 +144,7 @@ public:
             }
 
             worker->notify();
-            cout<<"workers: " <<workers_list.size()<< "SC: "<< SC_size()<<endl;
+            cout << "workers: " << workers_list.size() << "SC: " << SC_size() << endl;
         } while (not(workers_list.size() == num_cpu_workers + num_gpu_workers and data_array.empty() and is_SC_empty()));
         global_end_label = true;
         notify_all_workers();
